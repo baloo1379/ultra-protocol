c1b31ae921e398c84620a73c1ccb898db6cf643a
Jakub Florczak <44369186+flopczak@users.noreply.github.com>
2018-11-25 22:45:06 +0100

    moj klient

--- client.py
+++ client.py
@@ -4,24 +4,35 @@ import random
 import Protocol.protocol as proto
 HOST, PORT = "localhost", 9999
 data = input("message to send")
-
+basic = proto.Ultra()
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 packet = proto.Ultra(O=proto.CONNECTING, f=(proto.PUSH, proto.SYN), n = random.randrange(0,2000))
 ack_n = int(packet.flags_id[0]) + 1
-str(packet)
-sock.sendto(bytes(packet + "\n", "ascii"), (HOST, PORT))
+packet = str(packet)
+sock.sendto(bytes(packet, "ascii"), (HOST, PORT))
 
 
 
 rec = proto.Ultra()
-received = str(sock.recv(1024), "ascii")
-rec = rec.parse(received)
-resend = proto.Ultra(O=proto.CONNECTING, f=(proto.ACK, proto.SYN), n=ack_n)
+received = sock.recv(1024)
+received = str(received, "ascii")
+rec.parse(received)
+basic.session_id = rec.session_id
+
 if rec.proto.ACK == ack_n:
-    sock.sendto(bytes(resend + "\n", "ascii"), (HOST, PORT))
+    resend = proto.Ultra(O=proto.CONNECTING, f=(proto.ACK, proto.SYN), n=ack_n)
+    resend = str(resend)
+    sock.sendto(bytes(resend, "ascii"), (HOST, PORT))
+
+#while
+    range1 = proto.Ultra()
+    range = sock.recv(1024)
+    range = str(range, "ascii")
+    range1 = range1.parse(range)
+
 else:
     print("received ack is different than expected cannot connect")
 
